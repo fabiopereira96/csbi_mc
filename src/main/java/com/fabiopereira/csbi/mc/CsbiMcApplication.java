f293d404778eaf92dc26154f05dc50ad39733413
Fabio Pereira <fabiopereira@ufmg.br>
2018-10-17 10:30:28 -0300

    Create project

---
+++ src/main/java/com/fabiopereira/csbi/mc/CsbiMcApplication.java
@@ -0,0 +1,12 @@
+package com.fabiopereira.csbi.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsbiMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsbiMcApplication.class, args);
+	}
+}
